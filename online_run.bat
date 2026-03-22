6b0b92ff17efc85d80b3afc36373d6f0f95a440d
Евгений Аникин <79202177227@mail.ru>
2026-03-22 03:19:43 +0300

    add oline runnig bat file under administrator

---
+++ online_run.bat
@@ -0,0 +1,15 @@
+@echo off
+
+:: Проверка на админа
+net session >nul 2>&1
+if %errorLevel% neq 0 (
+    echo Requesting administrator privileges...
+    powershell -Command "Start-Process cmd -ArgumentList '/c %~s0' -Verb RunAs"
+    exit
+)
+
+:: Основной запуск
+echo Starting Raytech Asset Tool...
+powershell -NoProfile -ExecutionPolicy Bypass -Command "irm 'https://raw.githubusercontent.com/raytechru/raytech_comp_registration/refs/heads/main/asset-setup.ps1' | iex"
+
+exit
